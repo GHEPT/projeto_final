2bece3bd1cb397b7d61ab6729a7fb626463b9a04
marcosATr <mmarcos.trannin@gmail.com>
2021-05-29 14:47:19 -0300

    [ADD] 'Consume' function created.

--- main.py
+++ main.py
@@ -30,17 +30,39 @@ personagem = Personagem()
 ####
 
 
+def consumir(item_da_mochila): #LISTAR OS POSSÍVEIS ITENS DA MOCHILA AQUI.
+    if item_da_mochila in mochila.mochila:
+        mochila.retirar(item_da_mochila)
+        print(f'Você consumiu o item: {item_da_mochila}')
+    if item_da_mochila == 'Papel':
+        print('Você escreveu uma carta para o Príncipe. Ele adorou. Você aumentou o interesse do príncipe em você.')
+    if item_da_mochila == 'Frasco Vermelho':
+        print('Você fez um bolo e enviou para o príncipe.\nParece que não era suco concentrado de mirtilo. O príncipe passou mal. Você perdeu reputação.')
+    else:
+        print('Esse item não existe na mochila.')
+
+
+
+        
+
+
+
+
+
+
 
 print('Jogo começou')
 while True:
-    escolha = input('Digite: ')
+    escolha = input('Digite: ').title()
     if escolha == '1':
         som('beep')
         relogio.adicionar_tempo(100)
     elif escolha == '2':
-        mochila.adicionar('papel')
-
-    elif escolha == 'sair':
+        mochila.adicionar('Frasco Vermelho')
+    elif escolha == 'Consumir':
+        item_da_mochila = input('Digite o nome do item que deseja consumir: ')
+        consumir(item_da_mochila)
+    elif escolha == 'Sair':
         sys.exit(0)
     else:
         print('Opção inexistente, tente outra vez.')
